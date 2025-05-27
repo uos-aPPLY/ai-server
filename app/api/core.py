77bfa6f2b24296b6c68e53d2b7a411a3a578534f
nyungovo <nyungovo@gmail.com>
2025-05-27 14:20:04 +0900

    chore: check용 엔드포인트 추가

--- app/api/core.py
+++ app/api/core.py
@@ -5,5 +5,9 @@ from fastapi.responses import JSONResponse
 router = APIRouter()
 
 @router.get("/")
+async def check():
+    return JSONResponse(content={"status": "ok", "message": "AI Server is running."})
+
+@router.get("/healthcheck")
 async def healthcheck():
     return JSONResponse(content={"status": "ok", "message": "AI Server is running."})
