e47f4e6dfeb1c7e562485c4960337a2d8f644760
nyungovo <nyungovo@gmail.com>
2025-05-27 20:30:07 +0900

    test

--- app/api/core.py
+++ app/api/core.py
@@ -7,7 +7,3 @@ router = APIRouter()
 @router.get("/")
 async def check():
     return JSONResponse(content={"status": "ok", "message": "AI Server is running."})
-
-@router.get("/healthcheck")
-async def healthcheck():
-    return JSONResponse(content={"status": "ok", "message": "AI Server is running."})
