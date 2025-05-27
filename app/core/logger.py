6565925462bdcd5e6426d4fa42afe0d3130615e2
nyungovo <nyungovo@gmail.com>
2025-05-28 02:42:15 +0900

    chore: 로그 출력 시 타임스탬프도 출력하게 변경

--- app/core/logger.py
+++ app/core/logger.py
@@ -1,4 +1,11 @@
 import logging
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+    handlers=[
+        logging.FileHandler("fastapi.log"),
+        logging.StreamHandler()  # 콘솔에도 출력
+    ]
+)
 logger = logging.getLogger("app")
